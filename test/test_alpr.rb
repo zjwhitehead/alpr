e444b675bee01b71c2fe4e15b82bd87e84f41171
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2018-12-15 15:16:21 -0500

    Update test images for regions

--- test/test_alpr.rb
+++ test/test_alpr.rb
@@ -7,8 +7,18 @@ class TestAlpr < Minitest::Test
     refute_nil ::Alpr::VERSION
   end
 
-  def test_that_it_finds_the_license_plate_in_an_image
-    out = Alpr.new('test/lp.jpg')
-    assert out.output['results'].first['plate'].to_s == 'EAZ693'
+  def test_that_it_doesnt_find_us_plate_for_eu
+    out = Alpr.new('test/h786poj.jpg', :us)
+    assert out.output['results'].first['plate'].to_s != 'H786P0J'
+  end
+
+  def test_that_it_finds_the_license_plate_in_eu_image
+    out = Alpr.new('test/h786poj.jpg', :eu)
+    assert out.output['results'].first['plate'].to_s == 'H786P0J'
+  end
+
+  def test_that_it_finds_the_license_plate_in_us_image
+    out = Alpr.new('test/rubix.jpg', :us)
+    assert out.output['results'].first['plate'].to_s == 'RUBIX'
   end
 end
